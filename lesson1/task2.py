62d76c58b3f10ea2c89545071f9ec6fa47b282b4
kirillrzh <68221908+kirillrzh@users.noreply.github.com>
2020-10-20 12:20:44 +0300

    Update task2.py

--- lesson1/task2.py
+++ lesson1/task2.py
@@ -5,7 +5,7 @@ main_link = 'https://api.vk.com/method/groups.get'
 params = {
     'user_id' : '21092174',
     'v' : '5.124',
-    'access_token' : '5f4c6a135b63e64a8ee36ed5b841a9443f5ff6c1a7acaa5d502b930f2d762ec3eed5bcec919292a83d4ef',
+    'access_token' : '5f4c6a135b63e64a8ee36ed5b841a',
     'extendet' : '1'
 }
 response = requests.get(main_link,params=params,)
